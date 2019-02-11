f8dc51ceee54124af99a59e770461299887e0e68
Ara Winters <ara.winters@gmail.com>
2019-02-11 09:53:23 -0800

    more comments and updated drab theme to current.

--- projects/sdk-components-ng/src/lib/entity/detail/sz-entity-detail.component.ts
+++ projects/sdk-components-ng/src/lib/entity/detail/sz-entity-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit,  Input, Output, EventEmitter } from '@angular/core';
 import { SzSearchService } from '../../services/sz-search.service';
 import { distinctUntilChanged, map, tap } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 import {
   SzEntityData,
@@ -16,7 +17,6 @@ import {
 })
 export class SzEntityDetailComponent {
   private _entityId: number;
-  public projectId = 1;
   public entityDetailJSON: string = "";
 
   public entity: SzEntityData;
@@ -26,15 +26,51 @@ export class SzEntityDetailComponent {
   _possibleMatches: SzRelatedEntity[];
   _matches: SzEntityRecord[];
 
+  /**
+   * emitted when the component begins a request for an entities data.
+   * @returns entityId of the request being made.
+   */
+  @Output() requestStart: EventEmitter<number> = new EventEmitter<number>();
+  /**
+   * emitted when a search is done being performed.
+   * @returns the result of the entity request OR an Error object if something went wrong.
+   */
+  @Output() requestEnd: EventEmitter<SzEntityData|Error> = new EventEmitter<SzEntityData|Error>();
+  /**
+   * emitted when a search encounters an exception
+   * @returns the number of total results returned from the search.
+   */
+  @Output() exception: EventEmitter<Error> = new EventEmitter<Error>();
 
+  /**
+   * emmitted when the entity data to display has been changed.
+   */
+  @Output('dataChanged')
+  dataChanged: Subject<SzEntityData> = new Subject<SzEntityData>();
 
+  /**
+   * emmitted when the entity data to display has been changed.
+   */
+  @Input('data')
+  public set entityData(value: SzEntityData) {
+    this.entity = value;
+  }
+
+  /**
+   * set the entity data by passing in an entity id number.
+   */
   @Input()
   public set entityId(value: number) {
     this._entityId = value;
     this.onEntityIdChange();
   }
+  /**
+   * Get the entity Id of the current entity being displayed.
+   *
+   * @readonly
+   */
   public get entityId(): number {
-    return this._entityId;
+    return this.entity && this.entity.resolvedEntity && this.entity.resolvedEntity.entityId ? this.entity.resolvedEntity.entityId : this._entityId;
   }
 
   /**
@@ -76,16 +112,50 @@ export class SzEntityDetailComponent {
     }) : undefined;
   }
 
-  public onEntityRecordClick(entityId: number): void {
-    this.entityId = entityId;
-  }
-
   constructor(
     private searchService: SzSearchService,
   ) {}
 
+
+  /**
+   * after entity data has been changed, regenerate the filtered lists.
+   *  matches, possible matches, possible relationships, and disclosed relationships.
+   */
+  private onEntityDataChanged(){
+    // doing the set on these manually because pulling directly from setter(s)
+    // causes render change cycle to break mem and hammer redraw
+    if(this.entity && this.entity.resolvedEntity.records) this._matches = this.entity.resolvedEntity.records
+    if(this.entity && this.entity.relatedEntities.filter) this._possibleMatches = this.entity.relatedEntities.filter( (sr) => {
+      return sr.relationType == SzRelationshipType.POSSIBLEMATCH;
+    });
+    if(this.entity && this.entity.relatedEntities.filter) this._discoveredRelationships = this.entity.relatedEntities.filter( (sr) => {
+      return sr.relationType == SzRelationshipType.POSSIBLERELATION;
+    });
+    if(this.entity && this.entity.relatedEntities.filter) this._disclosedRelationships = this.entity.relatedEntities.filter( (sr) => {
+      return sr.relationType == SzRelationshipType.DISCLOSEDRELATION;
+    });
+
+    this.dataChanged.next(this.entity);
+  }
+
+  /**
+   * internal handler for when a entity record displayed inside of widget is
+   * clicked on.
+   */
+  public onEntityRecordClick(entityId: number): void {
+    this.entityId = entityId;
+  }
+
+  /**
+   * when entityId property is changed, request the data from the api
+   * and display the result.
+   *
+   * @memberof SzEntityDetailComponent
+   */
   onEntityIdChange() {
-    if (this.projectId > 0 && this._entityId) {
+    if (this._entityId) {
+      this.requestStart.emit(this._entityId);
+
       this.searchService.getEntityById(this._entityId).
       pipe(
         tap(res => console.log('SzSearchService.getEntityById: ' + this._entityId, res))
@@ -94,20 +164,12 @@ export class SzEntityDetailComponent {
         console.log('sz-entity-detail.onEntityIdChange: ', entityData);
         this.entityDetailJSON = JSON.stringify(entityData, null, 4);
         this.entity = entityData;
-
-        // doing the set on these manually because pulling directly from setter(s)
-        // causes render change cycle to break mem and hammer redraw
-        if(this.entity && this.entity.resolvedEntity.records) this._matches = this.entity.resolvedEntity.records
-        if(this.entity && this.entity.relatedEntities.filter) this._possibleMatches = this.entity.relatedEntities.filter( (sr) => {
-          return sr.relationType == SzRelationshipType.POSSIBLEMATCH;
-        });
-        if(this.entity && this.entity.relatedEntities.filter) this._discoveredRelationships = this.entity.relatedEntities.filter( (sr) => {
-          return sr.relationType == SzRelationshipType.POSSIBLERELATION;
-        });
-        if(this.entity && this.entity.relatedEntities.filter) this._disclosedRelationships = this.entity.relatedEntities.filter( (sr) => {
-          return sr.relationType == SzRelationshipType.DISCLOSEDRELATION;
-        });
-
+        this.onEntityDataChanged();
+        this.requestEnd.emit( entityData );
+        this.dataChanged.next( entityData );
+      }, (err)=>{
+        this.requestEnd.emit( err );
+        this.exception.next( err );
       });
     }
   }
