d4be93afb2453cc74d1d5d319ed49d0458ffd239
Ara Winters <ara.winters@gmail.com>
2019-06-05 09:15:48 -0700

    example fixes post-path-refactoring

--- examples/search-with-results-and-details/src/app/app.module.ts
+++ examples/search-with-results-and-details/src/app/app.module.ts
@@ -11,9 +11,9 @@ import { SzSearchComponentTest } from './search/sz-search/sz-search.component';
 
 import { SzSearchResultsTestComponent } from './search/sz-search-results-test/sz-search-results-test.component';
 import { SzSearchResultsCardTestComponent } from './search/sz-search-results-card-test/sz-search-results-card-test.component';
-import { SzRelationshipNetworkInputComponent } from 'projects/sdk-components-ng/src/lib/graph/sz-relationship-network-input/sz-relationship-network-input.component';
-import { SzRelationshipNetworkLookupComponent } from '../../../../projects/sdk-components-ng/src/lib/graph/sz-relationship-network-lookup/sz-relationship-network-lookup.component';
-import { SzRelationshipNetworkUploadComponent } from '../../../../projects/sdk-components-ng/src/lib/graph/sz-relationship-network-upload/sz-relationship-network-upload.component';
+import { SzRelationshipNetworkInputComponent } from '../../../../src/lib/graph/sz-relationship-network-input/sz-relationship-network-input.component';
+import { SzRelationshipNetworkLookupComponent } from '../../../../src/lib/graph/sz-relationship-network-lookup/sz-relationship-network-lookup.component';
+import { SzRelationshipNetworkUploadComponent } from '../../../../src/lib/graph/sz-relationship-network-upload/sz-relationship-network-upload.component';
 
 @NgModule({
   declarations: [
