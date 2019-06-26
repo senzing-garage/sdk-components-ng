7a609974104e9d2bf0d3d23b6370051bafbceea6
Ara Winters <ara.winters@gmail.com>
2019-06-26 08:22:55 -0700

    path refactoring. windows is dumb

--- examples/search-with-results-and-details/src/app/app.module.ts
+++ examples/search-with-results-and-details/src/app/app.module.ts
@@ -7,10 +7,6 @@ import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { SenzingSdkModule, SzRestConfiguration, SzPoweredByComponent  } from '@senzing/sdk-components-ng';
 
 import { AppComponent } from './app.component';
-import { SzSearchComponentTest } from './search/sz-search/sz-search.component';
-
-import { SzSearchResultsTestComponent } from './search/sz-search-results-test/sz-search-results-test.component';
-import { SzSearchResultsCardTestComponent } from './search/sz-search-results-card-test/sz-search-results-card-test.component';
 import { SzRelationshipNetworkInputComponent } from '../../../../src/lib/graph/sz-relationship-network-input/sz-relationship-network-input.component';
 import { SzRelationshipNetworkLookupComponent } from '../../../../src/lib/graph/sz-relationship-network-lookup/sz-relationship-network-lookup.component';
 import { SzRelationshipNetworkUploadComponent } from '../../../../src/lib/graph/sz-relationship-network-upload/sz-relationship-network-upload.component';
@@ -18,9 +14,6 @@ import { SzRelationshipNetworkUploadComponent } from '../../../../src/lib/graph/
 @NgModule({
   declarations: [
     AppComponent,
-    SzSearchComponentTest,
-    SzSearchResultsTestComponent,
-    SzSearchResultsCardTestComponent,
     SzRelationshipNetworkInputComponent,
     SzRelationshipNetworkLookupComponent,
     SzRelationshipNetworkUploadComponent
