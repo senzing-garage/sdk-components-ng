d0ea20763631808591d6cfa637527e2c87f63ecb
Ara Winters <ara.winters@gmail.com>
2019-07-25 17:01:25 -0700

    brute CI debugging.

--- src/lib/entity/detail/sz-entity-detail-header/content.component.ts
+++ src/lib/entity/detail/sz-entity-detail-header/content.component.ts
@@ -226,31 +226,29 @@ export class SzEntityDetailHeaderContentComponent implements OnInit {
   get identifierData(): string[] {
     console.log('SzEntityDetailHeaderContentComponent.identifierData #1');
     try{
-        console.log('SzEntityDetailHeaderContentComponent.identifierData #1.1');
-
-      if (this.entity) {
         console.log('SzEntityDetailHeaderContentComponent.identifierData #2');
 
+      if (this.entity) {
+        console.log('SzEntityDetailHeaderContentComponent.identifierData #3');
         if (this.entity.identifierData) {
-          console.log('SzEntityDetailHeaderContentComponent.identifierData #3');
-
+          console.log('SzEntityDetailHeaderContentComponent.identifierData #4');
           return this.entity.identifierData;
         } else if (this.entity.topIdentifiers) {
-          console.log('SzEntityDetailHeaderContentComponent.identifierData #4');
-
+          console.log('SzEntityDetailHeaderContentComponent.identifierData #5');
           return this.entity.topIdentifiers;
         } else {
+          console.log('SzEntityDetailHeaderContentComponent.identifierData #6');
           return [];
         }
       } else {
+        console.log('SzEntityDetailHeaderContentComponent.identifierData #7');
         return [];
       }
     }catch(err){
       console.log('\tSzEntityDetailHeaderContentComponent.identifierData error');
-
       console.log('\tSzEntityDetailHeaderContentComponent.identifierData error', err.message);
     }
-    console.log('SzEntityDetailHeaderContentComponent.identifierData #5');
+    console.log('SzEntityDetailHeaderContentComponent.identifierData #8');
     return [];
   }
 
