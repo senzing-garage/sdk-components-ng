63c5023cc965668fe37664bffbdd13b8f3093e00
Ara Winters <ara.winters@gmail.com>
2020-01-10 16:25:25 -0800

    tsdoc #133

--- src/lib/services/sz-bulk-data.service.ts
+++ src/lib/services/sz-bulk-data.service.ts
@@ -17,8 +17,8 @@ import { SzDataSourcesService } from './sz-datasources.service';
 import { SzAdminService } from './sz-admin.service';
 import { tap, map, catchError } from 'rxjs/operators';
 /**
- * methods used to manipulate datasources, entity types, entity classes,
- * and analyze and load data.
+ * methods used to manipulate data is bulk, ie
+ * import, analyze, map, and load data from a parseable format.
  *
  * @export
  */
@@ -26,33 +26,49 @@ import { tap, map, catchError } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class SzBulkDataService {
+  /** current file to analyze or load */
   public currentFile: File;
+  /**  current result of last analysis operation */
   public currentAnalysis: SzBulkDataAnalysis;
+  /** current result of last file load attempt */
   public currentLoadResult: SzBulkLoadResult;
   /** map of current datasource name to new datasource names */
   public dataSourceMap: { [key: string]: string };
   /** current datasources */
   _dataSources: string[];
-
+  /** when the file input changes this subject is broadcast */
   public onCurrentFileChange = new Subject<File>();
+  /** when the analysis result changes this behavior subject is broadcast */
   public onAnalysisChange = new BehaviorSubject<SzBulkDataAnalysis>(undefined);
+  /** when a datasrc destination changes this subject is broadcast */
   public onDataSourceMapChange = new Subject<{ [key: string]: string }>();
+  /** when the result of a load operation changes this behavior subject is broadcast */
   public onLoadResult = new BehaviorSubject<SzBulkLoadResult>(undefined);
+  /** when the file input changes this subject is broadcast */
   public onError = new Subject<Error>();
+  /** when a file is being analyzed */
   public analyzingFile = new Subject<boolean>();
+  /** when a file is being analyzed */
   public isAnalyzingFile = false;
+  /** when a file is being analyzed in the current thread */
   public loadingFile = new Subject<boolean>();
+  /** when a file is being loaded in to the engine on thread*/
   public isLoadingFile = false;
 
-
+  /** the file to analyze, map, or load */
   public set file(value: File) {
     this.currentFile = value;
     this.currentAnalysis = null;
     this.currentLoadResult = null;
     this.onCurrentFileChange.next( this.currentFile );
   }
+  /** the file being analyzed, mapped, or loaded */
   public get file(): File {
     return this.currentFile;
+  }
+  /** the datasources currently present */
+  private get dataSources(): string[] {
+    return this._dataSources;
   }
 
   constructor(
@@ -65,7 +81,7 @@ export class SzBulkDataService {
       this.analyzingFile.next(true);
 
       this.analyze(file).toPromise().then( (analysisResp: SzBulkDataAnalysisResponse) => {
-        console.log('autowire analysis resp on file change: ', analysisResp, this.currentAnalysis);
+        //console.log('autowire analysis resp on file change: ', analysisResp, this.currentAnalysis);
         this.analyzingFile.next(false);
       });
     });
@@ -76,31 +92,30 @@ export class SzBulkDataService {
       this.isLoadingFile = isLoading;
     });
     this.adminService.onServerInfo.subscribe((info) => {
-      console.log('ServerInfo obtained: ', info);
+      //console.log('ServerInfo obtained: ', info);
     });
     this.updateDataSources();
   }
-
-  private get dataSources(): string[] {
-    return this._dataSources;
-  }
+  /** update the internal list of datasources
+   * @internal
+   */
   private updateDataSources() {
     this.datasourcesService.listDataSources().subscribe((datasources: string[]) => {
-      console.log('datasources obtained: ', datasources);
+      //console.log('datasources obtained: ', datasources);
       this._dataSources = datasources.filter(s => s !== 'TEST' && s !== 'SEARCH');
     });
   }
-
+  /** create a new datasource */
   public createDataSources(dataSources: string[]): Observable<string[]> {
     console.log('SzBulkDataService.createDataSources: ', dataSources);
     return this.datasourcesService.addDataSources(dataSources);
   }
-
+  /** create a new entity type */
   public createEntityTypes(entityTypes: string[]): Observable<string[]> {
     console.log('SzBulkDataService.createEntityTypes: ', entityTypes, "ACTOR");
     return this.adminService.addEntityTypes(entityTypes, "ACTOR");
   }
-
+  /** analze a file and prep for mapping */
   public analyze(file: File): Observable<SzBulkDataAnalysisResponse> {
     console.log('SzBulkDataService.analyze: ', file);
     this.analyzingFile.next(true);
@@ -121,9 +136,12 @@ export class SzBulkDataService {
       })
     )
   }
-
+  /**
+   * load a files contents in to a datasource.
+   * @TODO show usage example.
+   */
   public load(file?: File, dataSourceMap?: { [key: string]: string }, analysis?: SzBulkDataAnalysis ): Observable<SzBulkLoadResult> | undefined {
-    console.log('SzBulkDataService.load: ', dataSourceMap, file);
+    //console.log('SzBulkDataService.load: ', dataSourceMap, file);
     file = file ? file : this.currentFile;
     dataSourceMap = dataSourceMap ? dataSourceMap : this.dataSourceMap;
     analysis = analysis ? analysis : this.currentAnalysis;
@@ -135,15 +153,6 @@ export class SzBulkDataService {
       }).map( (b) => {
         return this.dataSourceMap[b.dataSource];
       });
-      /*
-      this.currentAnalysis.analysisByDataSource.forEach(a => {
-        const targetDS = this.dataSourceMap[a.dataSource];
-        if (targetDS && this._dataSources.indexOf(targetDS) < 0) {
-          newDataSources.push(targetDS);
-        }
-      });
-      */
-      console.log('handleFileLoad: new datasources', newDataSources);
 
       let promise = Promise.resolve([]);
       let retVal: Subject<SzBulkLoadResult> =  new Subject<SzBulkLoadResult>();
@@ -164,7 +173,7 @@ export class SzBulkDataService {
             return of(undefined);
           }),
           tap((response: SzBulkLoadResponse) => {
-            console.log('RESPONSE', dataSourceMap, response.data);
+            //console.log('RESPONSE', dataSourceMap, response.data);
             this.currentLoadResult = response.data;
             this.onLoadResult.next( this.currentLoadResult );
             this.loadingFile.next(false);
@@ -182,38 +191,10 @@ export class SzBulkDataService {
       return;
     }
   }
-  /*
-  public loaderrr(event: Event) {
-    const newDataSources = [];
-    if (this.currentAnalysis) {
-
-      this.currentAnalysis.analysisByDataSource.forEach(a => {
-        const targetDS = this.dataSourceMap[a.dataSource];
-        if (targetDS && this._dataSources.indexOf(targetDS) < 0) {
-          newDataSources.push(targetDS);
-        }
-      });
-
-      console.log('handleFileLoad: new datasources', newDataSources);
-
-      let promise = Promise.resolve([]);
-      if (newDataSources.length > 0) {
-        const p1 = this.createDataSources(newDataSources).toPromise();
-        const p2 = this.createEntityTypes(newDataSources).toPromise();
-        promise = Promise.all([p1, p2]);
-      }
-      promise.then(() => {
-        this.bulkDataService.load(
-          this.currentFile,
-          this.bulkDataAnalysisComponent.dataSourceMap).toPromise().then(response => {
-          console.log('RESPONSE', this.bulkDataAnalysisComponent.dataSourceMap, response);
-          this.loadResult = response.data;
-        });
-
-      });
-    }
-  }*/
-
+  /**
+   * Used to keep a internal map of source to target datasource names.
+   * @internal
+   */
   public getDataSourceMapFromAnalysis(analysisArray: SzDataSourceRecordAnalysis[]): { [key: string]: string } {
     let _dsMap: { [key: string]: string } = {};
     analysisArray.forEach(a => {
@@ -225,13 +206,15 @@ export class SzBulkDataService {
     });
     return _dsMap;
   }
-
+  /**
+   * change the destination datasource of a file currently being mapped to datasource.
+   */
   public changeDataSourceName(fromDataSource: string, toDataSource: string) {
     console.log('MAP ' + fromDataSource + ' TO ' + toDataSource, this.dataSourceMap);
     this.dataSourceMap = this.dataSourceMap
     this.dataSourceMap[fromDataSource] = toDataSource;
   }
-
+  /** clear any file and associated data. removes file focus context */
   public clear(): void {
     this.currentAnalysis = undefined;
     this.currentLoadResult = undefined;
