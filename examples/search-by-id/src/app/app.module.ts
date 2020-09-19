c369fca24b6aba8accc2475f20acdc69ee33e7c8
Ara Winters <ara.winters@gmail.com>
2020-09-18 19:35:52 -0700

    #143 example cleanup

--- examples/search-by-id/src/app/app.module.ts
+++ examples/search-by-id/src/app/app.module.ts
@@ -3,12 +3,32 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 // import { CommonModule } from '@angular/common';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { SenzingSdkModule, SzPrefsService, SzConfigurationService } from '@senzing/sdk-components-ng';
+import { SenzingSdkModule, SzPrefsService, SzConfigurationService, SzRestConfiguration } from '@senzing/sdk-components-ng';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { StorageServiceModule } from 'ngx-webstorage-service';
 import { NgxJsonViewerModule } from 'ngx-json-viewer';
 import { AppComponent } from './app.component';
 import { SzPrefsManagerComponent } from './prefs/prefs-manager.component';
+
+/**
+* Pull in api configuration(SzRestConfigurationParameters)
+* from: environments/environment
+*
+* @example
+* ng build -c production
+* ng serve -c docker
+*/
+import { apiConfig, environment } from './../environments/environment';
+
+/**
+ * create exportable config factory
+ * for AOT compilation.
+ *
+ * @export
+ */
+export function SzRestConfigurationFactory() {
+  return new SzRestConfiguration( (apiConfig ? apiConfig : undefined) );
+}
 
 @NgModule({
   declarations: [
