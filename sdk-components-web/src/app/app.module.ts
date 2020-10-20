f0e18e229085ca97e22a97295dca5d07b37ba01d
Ara Winters <ara.winters@gmail.com>
2020-10-19 19:43:57 -0700

    #169 added missing components. sans small graph and graph tags. will handle separately.

--- sdk-components-web/src/app/app.module.ts
+++ sdk-components-web/src/app/app.module.ts
@@ -5,14 +5,18 @@ import { APP_BASE_HREF } from '@angular/common';
 
 import {
   SenzingSdkModule,
-  SzSearchComponent,
-  SzSearchResultsComponent,
-  SzEntityDetailComponent,
-  SzRestConfiguration,
-  SzPoweredByComponent,
   SzConfigurationAboutComponent,
   SzConfigurationComponent,
-  SzPreferencesComponent
+  SzEntityDetailComponent,
+  SzEntityDetailGraphComponent,
+  SzEntityRecordViewerComponent,
+  SzPoweredByComponent,
+  SzPreferencesComponent,
+  SzRestConfiguration,
+  SzSearchByIdComponent,
+  SzSearchComponent,
+  SzSearchResultsComponent,
+  SzStandaloneGraphComponent
 } from '@senzing/sdk-components-ng';
 // for graph support
 import {
@@ -53,33 +57,60 @@ export function SzRestConfigurationFactory() {
 })
 export class AppModule { 
   constructor(private injector: Injector) {
-    // search box
-    const el1 = createCustomElement(SzSearchComponent, { injector });
-    customElements.define('sz-search', el1);
-    // search results list
-    const el2 = createCustomElement(SzSearchResultsComponent, { injector });
-    customElements.define('sz-search-results', el2);
-    // entity detail
-    const el3 = createCustomElement(SzEntityDetailComponent, { injector });
-    customElements.define('sz-entity-detail', el3);
-    // configuration injector
-    const el4 = createCustomElement(SzConfigurationComponent, { injector });
-    customElements.define('sz-configuration', el4);
+
     // configuration summary
-    const el5 = createCustomElement(SzConfigurationAboutComponent, { injector });
-    customElements.define('sz-configuration-about', el5);
+    const WC_CONF_ABOUT = createCustomElement(SzConfigurationAboutComponent, { injector });
+    customElements.define('sz-configuration-about', WC_CONF_ABOUT);
+
     // configuration injector
-    const el6 = createCustomElement(SzPreferencesComponent, { injector });
-    customElements.define('sz-preferences', el6);
+    const WC_CONF = createCustomElement(SzConfigurationComponent, { injector });
+    customElements.define('sz-configuration', WC_CONF);
+
+    // entity detail
+    const WC_ENT_DETAIL = createCustomElement(SzEntityDetailComponent, { injector });
+    customElements.define('sz-entity-detail', WC_ENT_DETAIL);
+
+    // small embedded graph 
+    // ! breaks entity detail graph. not sure why
+    //const WC_GRAPH_SMALL = createCustomElement(SzEntityDetailGraphComponent, { injector });
+    //customElements.define('sz-entity-detail-graph', WC_GRAPH_SMALL);
+
+    // view a entity record information (json tree/summary)
+    const WC_ENT_REC_VIEWER = createCustomElement(SzEntityRecordViewerComponent, { injector });
+    customElements.define('sz-entity-record-viewer', WC_ENT_REC_VIEWER);
+
     // powered by tag
-    const el7 = createCustomElement(SzPoweredByComponent, { injector });
-    customElements.define('sz-powered-by', el7);
+    const WC_PWR_BY = createCustomElement(SzPoweredByComponent, { injector });
+    customElements.define('sz-powered-by', WC_PWR_BY);
+
+    // preferences control component
+    const WC_PREFS_PNL = createCustomElement(SzPreferencesComponent, { injector });
+    customElements.define('sz-preferences', WC_PREFS_PNL);
+
+    // search by record id or entity id
+    const WC_SRCH_BY_ID = createCustomElement(SzSearchByIdComponent, { injector });
+    customElements.define('sz-search-by-id', WC_SRCH_BY_ID);
+
+    // search by attribute or name
+    const WC_SRCH_BY_ATTR = createCustomElement(SzSearchComponent, { injector });
+    customElements.define('sz-search', WC_SRCH_BY_ATTR);
+
+    // search results list
+    const WC_SRCH_RESULTS = createCustomElement(SzSearchResultsComponent, { injector });
+    customElements.define('sz-search-results', WC_SRCH_RESULTS);
+    
+    // large format graph
+    const WC_GRAPH_LARGE = createCustomElement(SzStandaloneGraphComponent, { injector });
+    customElements.define('sz-standalone-graph', WC_GRAPH_LARGE);
+    
     // relationship network graph tag
-    // const el7 = createCustomElement(SzRelationshipNetworkComponent, {injector});
-    // customElements.define('sz-relationship-network', el7);
+    // ! breaks entity detail graph. not sure why
+    //const WC_GRAPH_NETWORK = createCustomElement(SzRelationshipNetworkComponent, { injector });
+    //customElements.define('sz-relationship-network', WC_GRAPH_NETWORK);
+    
     // relationship path graph tag
-    // const el8 = createCustomElement(SzRelationshipPathComponent, {injector});
-    // customElements.define('sz-relationship-path', el8);
+    //const WC_GRAPH_PATH = createCustomElement(SzRelationshipPathComponent, {injector});
+    //customElements.define('sz-relationship-path', WC_GRAPH_PATH);
   }
   ngDoBootstrap() {}
 }
